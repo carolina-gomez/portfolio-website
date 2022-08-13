6448901dcad00af0dc7b26c5990a698e2f08e411
Carolina Gomez <carolinagomezTX@gmail.com>
2022-08-13 01:24:03 -0500

    navbar additional css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css"
 
 function Navbar() {
   return (
     <div className='navbar'>
-
         <div className='toggleButton'>
-            <button></button>
+            <button>Button</button>
         </div>
         <div className='links'>
             <Link to='/'>Home</Link>
             <Link to='/projects'>Projects</Link>
             <Link to='/experience'>Experience</Link>
+            <div className='dot'></div>
         </div>
     </div>
   )
