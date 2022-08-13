a9a9f8ece14dd80292425b38e2487d55b92af7aa
Carolina Gomez <carolinagomezTX@gmail.com>
2022-08-13 01:53:36 -0500

    home page started

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import LinkedIn from '@material-ui/icons/LinkedIn'
+import Email from '@material-ui/icons/Email'
+import GitHub from '@material-ui/icons/GitHub'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2>Hi, My name is Carolina</h2>
+        <div className='prompt'>
+          <p> 
+            A software developer with a passion for learning and creating.
+          </p>
+          <LinkedIn />
+          <Email />
+          <GitHub />
+        </div>
+      </div>
+      <div className='skills'></div>
+    </div>
   )
 }
 
