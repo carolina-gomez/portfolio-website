59caaddf7388e89a44f43adb48bf3da3ccd9142c
Carolina Gomez <carolinagomezTX@gmail.com>
2022-08-12 21:16:59 -0500

    imported files on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
 
 function App() {
   return <div className="App">
@@ -7,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/projects" element={<Projects/>} />
-        <Route path="/experience" element={<Experence/>} />
+        <Route path="/experience" element={<Experience/>} />
       </Routes>
     </Router>
   </div>;
