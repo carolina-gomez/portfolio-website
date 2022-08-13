9ac26aa32138d4ed88b58cc4c18045f131e719ea
Carolina Gomez <carolinagomezTX@gmail.com>
2022-08-13 01:34:51 -0500

    navbar responsive logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css"
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
   return (
-    <div className='navbar'>
+    <div className='navbar' id={expandNavbar ? "open" : "close"}>
         <div className='toggleButton'>
-            <button>Button</button>
+            <button onClick={() => {
+                setExpandNavbar((prev) => !prev)
+            }}>
+                <ReorderIcon/>
+            </button>
         </div>
         <div className='links'>
             <Link to='/'>Home</Link>
