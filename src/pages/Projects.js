6678b80461f4c3cd654d2ae43827f3cf5125f9d2
Carolina Gomez <carolinagomezTX@gmail.com>
2022-08-13 14:56:30 -0500

    Projects.js and ProjectIrem.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import ProjectItem from './ProjectItem'
+import ecommerceimg from "../assets/proj2.jpg"
+import spotifyimg from "../assets/proj3.webp"
+import "../styles/Projects.css"
+
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1>My Personal Projects</h1>
+      <div className='projectList'>
+        <ProjectItem name="E-commerce site" image={ecommerceimg}/>
+        <ProjectItem name="Spotify clone" image={spotifyimg}/>
+      </div>
+    </div>
   )
 }
 
