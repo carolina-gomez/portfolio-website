985486ff138e3f070dc486a65f5cd720270a1035
Carolina Gomez <carolinagomezTX@gmail.com>
2022-08-12 21:15:00 -0500

    created routes and files for Home, Projects and Experience

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/experience" element={<Experence/>} />
+      </Routes>
+    </Router>
+  </div>;
 }
 
 export default App;
