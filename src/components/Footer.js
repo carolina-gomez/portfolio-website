cec44ea6913bc0151f692f6a1fe3d6bfa8075ab0
Carolina Gomez <carolinagomezTX@gmail.com>
2022-08-13 01:41:50 -0500

    footer setup

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer">
+        <div>
+            <InstagramIcon />
+            <TwitterIcon />
+            <FacebookIcon />
+            <LinkedInIcon />
+        </div>
+        <p> &copy; 2022 Carolina Gomez</p>
+    </div>
+  )
+}
+
+export default Footer
